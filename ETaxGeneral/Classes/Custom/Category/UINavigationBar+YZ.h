c53278030395042d888393f45d7d840c8e69355e
micyo202 <micyo202@163.com>
2018-03-07 10:51:59 +0800

    自定义导航栏

--- ETaxGeneral/Classes/Custom/Category/UINavigationBar+YZ.h
+++ ETaxGeneral/Classes/Custom/Category/UINavigationBar+YZ.h
@@ -4,18 +4,40 @@
  Company  : Prient
  Author   : Yanzheng 严正
  Date     : 2017-10-26
- Version  : 1.0
+ LastDate : 2018-03-06
+ Version  : 2.0
  Declare  : Copyright © 2017 Yanzheng. All rights reserved.
  ************************************************************/
-/*
+
 #import <UIKit/UIKit.h>
 
 @interface UINavigationBar (YZ)
 
+// version 1.0：封装导航栏自定义方法（废弃）
+/*
 - (void)yz_setBackgroundColor:(UIColor *)backgroundColor;
 - (void)yz_setElementsAlpha:(CGFloat)alpha;
 - (void)yz_setTranslationY:(CGFloat)translationY;
 - (void)yz_reset;
+*/
+
+// version 2.0：封装导航栏自定义方法
+
+/**
+ * 初始化导航栏，隐藏导航栏下的横线，背景色置空
+ */
+- (void)yz_initialize;
+
+/**
+ * @param color 最终显示的颜色
+ * @param scrollView 当前滑动的视图
+ * @param value 滑动临界值，根据情况设定
+ */
+- (void)yz_changeColor:(UIColor *)color WithScrollView:(UIScrollView *)scrollView AndValue:(CGFloat)value;
+
+/**
+ * 还原导航栏
+ */
+- (void)yz_reset;
 
 @end
-*/
--- ETaxGeneral/Classes/Custom/Category/UINavigationBar+YZ.m
+++ ETaxGeneral/Classes/Custom/Category/UINavigationBar+YZ.m
@@ -4,15 +4,17 @@
  Company  : Prient
  Author   : Yanzheng 严正
  Date     : 2017-10-26
- Version  : 1.0
+ LastDate : 2018-03-06
+ Version  : 2.0
  Declare  : Copyright © 2017 Yanzheng. All rights reserved.
  ************************************************************/
 
-/*
 #import "UINavigationBar+YZ.h"
 
 @implementation UINavigationBar (YZ)
 
+// version 1.0：封装导航栏自定义方法（废弃）
+/*
 static char overlayKey;
 
 - (UIView *)overlay
@@ -71,6 +73,54 @@ static char overlayKey;
     [self.overlay removeFromSuperview];
     self.overlay = nil;
 }
+*/
+
+// version 2.0：封装导航栏自定义方法
+- (void)yz_initialize {
+    UIImageView *shadowImg = [self findNavLineImageViewOn:self];
+    shadowImg.hidden = YES;
+    self.translucent = YES;
+}
+
+- (void)yz_changeColor:(UIColor *)color WithScrollView:(UIScrollView *)scrollView AndValue:(CGFloat)value {
+    if (scrollView.contentOffset.y < 0) {
+        //下拉时导航栏隐藏
+        self.hidden = YES;
+    }else {
+        self.hidden = NO;
+        //计算透明度
+        CGFloat alpha = scrollView.contentOffset.y /90 >1.0f ? 1:scrollView.contentOffset.y/90;
+        //设置一个颜色并转化为图片
+        UIImage *image = [UIImage imageWithColor:[color colorWithAlphaComponent:alpha]];
+        if(alpha == 1){
+            self.translucent = NO;
+        }else{
+            self.translucent = YES;
+        }
+        [self setBackgroundImage:image forBarMetrics:UIBarMetricsDefault];
+    }
+}
+
+- (void)yz_reset {
+    UIImageView *shadowImg = [self findNavLineImageViewOn:self];
+    shadowImg.hidden = NO;
+    [self setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+}
+
+/**
+ * 寻找导航栏下的横线（私有方法）
+ */
+- (UIImageView *)findNavLineImageViewOn:(UIView *)view {
+    if ([view isKindOfClass:UIImageView.class] && view.bounds.size.height <= 1.0) {
+        return (UIImageView *)view;
+    }
+    for (UIView *subview in view.subviews) {
+        UIImageView *imageView = [self findNavLineImageViewOn:subview];
+        if (imageView) {
+            return imageView;
+        }
+    }
+    return nil;
+}
 
 @end
-*/
